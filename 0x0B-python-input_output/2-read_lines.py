73b1d69d2bc3145f4eee02cf6e540b549017c53f
bollilia <bollykevin75@gmail.com>
2022-09-27 11:31:23 -0700

    initial upload with completed tasks

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" Module that contains a function that appends to a text file
+"""
+Contains the "read_lines" function
 """
 
 
-def append_write(filename="", text=""):
-    """ Function that appends to a text file
-    Args:
-        filename: filename
-        text: text to write
-    Raises
-        Exception: when the file can be opened
-    """
-
-    with open(filename, 'a', encoding="utf-8") as f:
-        return f.write(text)
+def read_lines(filename="", nb_lines=0):
+    """reads n lines of a text file (UTF8) and prints it to stdout"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        if nb_lines <= 0:
+            print(f.read(), end='')
+            return
+        i = 0
+        for i, line in enumerate(f):
+            if i == nb_lines:
+                break
+            print(line, end='')
